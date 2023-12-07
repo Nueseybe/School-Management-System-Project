8adab72e32aaf8a7f3725655f6b6d04e06e951da
Nueseybe <karakurtnuseybe@gmail.com>
2023-12-07 15:44:09 +0100

    imported user-info.js file to app.js file and edited acs.init function

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import mainTemplate from './modules/main-template.js';
 import navbarMenu from './modules/navbar-menu.js';
 import sideMenu from './modules/side-menu.js';
-import footer from './modules/footer.js';
+import userInfo from './modules/user-info.js';
+import footer from './modules/footer.js'
 
 const acs ={};
 acs.init = () =>{
     mainTemplate.init();
     mainTemplate.addNavbarMenu(navbarMenu.render());
     mainTemplate.addSidebarMenu(sideMenu.render());
+    mainTemplate.addUserInfo(userInfo.render());
     mainTemplate.addFooter(footer.render());
     mainTemplate.addEventListener();
 }
