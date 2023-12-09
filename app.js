ef8a384cb66d00b3471ffc0481f1a330d153570b
Nueseybe <karakurtnuseybe@gmail.com>
2023-12-09 22:45:20 +0100

    added addMain functions for homepage main part

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import mainTemplate from './modules/main-template.js';
 import navbarMenu from './modules/navbar-menu.js';
 import sideMenu from './modules/side-menu.js';
-import footer from './modules/footer.js'
+import footer from './modules/footer.js';
+import home from  './modules/home.js';
 
 const acs ={};
 acs.init = () =>{
@@ -9,6 +10,7 @@ acs.init = () =>{
     mainTemplate.addNavbarMenu(navbarMenu.render());
     mainTemplate.addSidebarMenu(sideMenu.render());
     mainTemplate.addFooter(footer.render());
+    mainTemplate.addMain(home.render());
     mainTemplate.addEventListenerSideBar(),
     mainTemplate.addEventListenerNavBar()
 }
