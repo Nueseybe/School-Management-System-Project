55c7146bcb795db858a51e73bc12cbeb0ba01c34
Nueseybe <karakurtnuseybe@gmail.com>
2023-12-09 02:33:13 +0100

    edited teachers page content

--- templates/pages/teachers.js
+++ templates/pages/teachers.js
@@ -1,3 +1,13 @@
-export default function template(employee){
-    return ``
+export default function teachersItems(item){
+    return `
+
+    <h1> TEACHERS PAGE </h1>
+    <div class="card content-column-body m-5" style="width: 18rem;">
+        <div class="card-body ">
+        <h4 class="card-title"></h5>
+        <p class="card-text"></p>
+        <a href="#" class="btn btn-primary">Details</a>
+        </div>
+    </div>
+    `
 }
