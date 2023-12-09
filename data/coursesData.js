385aeb8f5f444a3570d08c4df74a73af7b7442e8
Nueseybe <karakurtnuseybe@gmail.com>
2023-12-09 02:06:49 +0100

    created data folder and created classesData.js, coursesData.js, studentsData.js and teachersData.js

---
+++ data/coursesData.js
@@ -0,0 +1,73 @@
+export default coursesData = [
+    {
+        className: "Web Development",
+        details: [{
+            course101A: [{
+                courseName : "HTML",
+                time : "6 Weeks",
+                information: ""
+            }],
+            course101B:  [{
+                courseName : "CSS",
+                time : "4 Weeks",
+                information: ""
+            }],
+            course101C:  [{
+                courseName : "JavaScript",
+                time : "10 Weeks",
+                information: ""
+            }]
+        }],
+        className: "Python",
+        details: [{
+            course102A: [{
+                courseName : "Python OOP",
+                time : "8 Weeks",
+                information: ""
+            }],
+            course101B:  [{
+                courseName : "PyQT5",
+                time : "3 Weeks",
+                information: ""
+            }]
+        }],
+        className: "Data Science",
+        details: [{
+            course101A: [{
+                courseName : "Preprocessing",
+                time : "4 Weeks",
+                information: ""
+            }],
+            course101B:  [{
+                courseName : "Litheratur",
+                time : "3 Weeks",
+                information: ""
+            }],
+            course101C:  [{
+                courseName : "Modeling",
+                time : "7 Weeks",
+                information: ""
+            }]
+        }],
+        className: "Aws-Cloud",
+        details: [{
+            course101A: [{
+                courseName : "JAM",
+                time : "6 Weeks",
+                information: ""
+            }],
+            course101B:  [{
+                courseName : "EC2",
+                time : "6 Weeks",
+                information: ""
+            }],
+            course101C:  [{
+                courseName : "ELB & ASG",
+                time : "6 Weeks",
+                information: ""
+            }]
+        }]
+
+    }
+]
+
