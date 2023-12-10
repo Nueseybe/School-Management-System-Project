8c7e8eacd1d91fd2123c556b1e50b8f5cdd5c52a
Nueseybe <karakurtnuseybe@gmail.com>
2023-12-10 23:39:18 +0100

    edited studentsData.js file

--- data/studentsData.js
+++ data/studentsData.js
@@ -1,94 +1,170 @@
 const studentsData = [
-    { 
-        id: 901, 
-        name: "John Smith", 
-        courses: 
-        ["Web Development", "Data Science"] 
+    {
+        id: 901,
+        name: "John Smith",
+        class: "Web Engineering",
+        courses: [
+            { name: "HTML", grade: 7 },
+            { name: "CSS", grade: 6 },
+            { name: "JavaScript", grade: 8 }
+        ],
+        info: "Some quick example text to build on the card title and make up the bulk of the card's content."
     },
-    { 
-        id: 902, 
-        name: "Mehmet Demir", 
-        courses: 
-        ["Python", "AWS Cloud"] 
+    {
+        id: 902,
+        name: "Mehmet Demir",
+        class: "AWS Cloud",
+        courses: [
+            { name: "JAM", grade: 4 },
+            { name: "EC2", grade: 5 },
+            { name: "ELB & ASG", grade: 7 }
+        ],
+        info: "Some quick example text to build on the card title and make up the bulk of the card's content."
     },
-    { 
-        id: 903, 
-        name: "Michael Davis", 
-        courses: 
-        ["Web Development", "AWS Cloud"] 
+    {
+        id: 903,
+        name: "Michael Davis",
+        class: "Web Engineering",
+        courses: [
+            { name: "HTML", grade: 7 },
+            { name: "CSS", grade: 6 },
+            { name: "JavaScript", grade: 8 }
+        ],
+        info: "Some quick example text to build on the card title and make up the bulk of the card's content."
     },
-    { 
-        id: 904, 
-        name: "Olivia Wilson", 
-        courses: 
-        ["Data Science", "Python"] 
+    {
+        id: 904,
+        name: "Olivia Wilson",
+        class: "Data Science",
+        courses: [
+            { name: "Preprocessing", grade: 5 },
+            { name: "Litheratur", grade: 6 },
+            { name: "Modelling", grade: 8 }
+        ],
+        info: "Some quick example text to build on the card title and make up the bulk of the card's content."
+
     },
-    { 
-        id: 905, 
-        name: "James Miller", 
-        courses: 
-        ["Web Development", "Python"] 
+    {
+        id: 905,
+        name: "James Miller",
+        class: "Web Engineering",
+        courses: [
+            { name: "HTML", grade: 7 },
+            { name: "CSS", grade: 6 },
+            { name: "JavaScript", grade: 8 }
+        ],
+        info: "Some quick example text to build on the card title and make up the bulk of the card's content."
     },
-    { 
-        id: 906, 
-        name: "Ali Tekin", 
-        courses: 
-        ["Data Science", "AWS Cloud"] 
+    {
+        id: 906,
+        name: "Ali Tekin",
+        class: "AWS Cloud",
+        courses: [
+            { name: "JAM", grade: 4 },
+            { name: "EC2", grade: 5 },
+            { name: "ELB & ASG", grade: 7 }
+        ],
+        info: "Some quick example text to build on the card title and make up the bulk of the card's content."
     },
-    { 
-        id: 907, 
-        name: "William Taylor", 
-        courses: 
-        ["Web Development", "Data Science"] 
+    {
+        id: 907,
+        name: "William Taylor",
+        class: "Web Engineering",
+        courses: [
+            { name: "HTML", grade: 7 },
+            { name: "CSS", grade: 6 },
+            { name: "JavaScript", grade: 8 }
+        ],
+        info: "Some quick example text to build on the card title and make up the bulk of the card's content."
     },
-    { 
-        id: 908, 
-        name: "Isabella Anderson", 
-        courses: 
-        ["Python", "AWS Cloud"] 
+    {
+        id: 908,
+        name: "Isabella Anderson",
+        class: "Python",
+        courses: [
+            { name: "Python OOP", grade: 5 },
+            { name: "PyQT5", grade: 7 },
+            { name: "Scrapy", grade: 7 }
+        ],
+        info: "Some quick example text to build on the card title and make up the bulk of the card's content."
     },
-    { 
-        id: 909, 
-        name: "David Martinez", 
-        courses: 
-        ["Web Development", "AWS Cloud"] 
+    {
+        id: 909,
+        name: "David Martinez",
+        class: "Web Engineering",
+        courses: [
+            { name: "HTML", grade: 7 },
+            { name: "CSS", grade: 6 },
+            { name: "JavaScript", grade: 8 }
+        ],
+        info: "Some quick example text to build on the card title and make up the bulk of the card's content."
     },
-    { 
-        id: 910, 
-        name: "Ava Harris", 
-        courses: 
-        ["Data Science", "Python"] 
+    {
+        id: 910,
+        name: "Ava Harris",
+        class: "Data Science",
+        courses: [
+            { name: "Preprocessing", grade: 5 },
+            { name: "Litheratur", grade: 6 },
+            { name: "Modelling", grade: 8 }
+        ],
+        info: "Some quick example text to build on the card title and make up the bulk of the card's content."
     },
-    { 
-        id: 911, 
-        name: "Liam Jackson", 
-        courses: 
-        ["Web Development", "Python"] 
+    {
+        id: 911,
+        name: "Liam Jackson",
+        class: "Python",
+        courses: [
+            { name: "Python OOP", grade: 5 },
+            { name: "PyQT5", grade: 7 },
+            { name: "Scrapy", grade: 7 }
+        ],
+        info: "Some quick example text to build on the card title and make up the bulk of the card's content."
     },
-    { 
-        id: 912, 
-        name: "Emily White", 
-        courses: 
-        ["Data Science", "AWS Cloud"] 
+    {
+        id: 912,
+        name: "Emily White",
+        class: "Data Science",
+        courses: [
+            { name: "Preprocessing", grade: 5 },
+            { name: "Litheratur", grade: 6 },
+            { name: "Modelling", grade: 8 }
+        ],
+        info: "Some quick example text to build on the card title and make up the bulk of the card's content."
     },
-    { 
-        id: 913, 
-        name: "Benjamin Thompson", 
-        courses: 
-        ["Web Development", "Data Science"] 
+    {
+        id: 913,
+        name: "Benjamin Thompson",
+        class: "Web Engineering",
+        courses: [
+            { name: "HTML", grade: 7 },
+            { name: "CSS", grade: 6 },
+            { name: "JavaScript", grade: 8 }
+        ],
+        info: "Some quick example text to build on the card title and make up the bulk of the card's content."
     },
-    { 
-        id: 914, 
-        name: "Mia Harris", 
-        courses: 
-        ["Python", "AWS Cloud"] 
+    {
+        id: 914,
+        name: "Mia Harris",
+        class: "Python",
+        courses: [
+            { name: "Python OOP", grade: 5 },
+            { name: "PyQT5", grade: 7 },
+            { name: "Scrapy", grade: 7 }
+        ],
+        info: "Some quick example text to build on the card title and make up the bulk of the card's content."
     },
-    { 
-        id: 915, 
-        name: "Alexander Clark", 
-        courses: 
-        ["Web Development", "AWS Cloud"] 
+    {
+        id: 915,
+        name: "Alexander Clark",
+        class: "Web Engineering",
+        courses: [
+            { name: "HTML", grade: 7 },
+            { name: "CSS", grade: 6 },
+            { name: "JavaScript", grade: 8 }
+        ],
+        info: "Some quick example text to build on the card title and make up the bulk of the card's content."
     }
 ]
 
-export default studentsData;
+export default studentsData
