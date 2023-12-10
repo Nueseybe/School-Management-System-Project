74d3960d2ae6e55184a907c2cd3961c93877a425
Nueseybe <karakurtnuseybe@gmail.com>
2023-12-10 20:25:35 +0100

    edited html codes in students.js

--- templates/pages/students.js
+++ templates/pages/students.js
@@ -1,29 +1,12 @@
 export default function studentItems (item){
     return `
-
-        
-    <h1> STUDENTS PAGE </h1>
-    <div class="row">
-        <div class="col-md-5 title">
-            <h2> Welcome student.name</h2>
-        </div>
-        <div class="col-md-7 d-flex align-item-end justify-content-end ">
-            <div class="row">
-                <img src="./images/student_icon/user-icon-03.png">
+        <div class="card content-column-body m-4" style="width: 18rem;">
+            <div class="card-body ">
+                <h4 class="card-title">${student.name}</h4>
+                    <p class="card-text">${student.courses}</p>
+                <a href="#" class="btn btn-primary" id="edit-student">Edit</a>
             </div>
         </div>
-        <div class="row ">
-            <p class="d-flex justify-content-end">student.name</p>
-            <p class="d-flex justify-content-end">logout</p>
-        </div>
-    </div>
-    <div class="card content-column-body m-5" style="width: 18rem;">
-        <div class="card-body ">
-        <h4 class="card-title"></h5>
-        <p class="card-text"></p>
-        <a href="#" class="btn btn-primary">Details</a>
-        </div>
-    </div>
         
     `
 }
