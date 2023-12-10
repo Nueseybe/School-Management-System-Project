6affaa7c7bbf807cf57c4f2e85b3412ed13dcd0f
Nueseybe <karakurtnuseybe@gmail.com>
2023-12-10 20:52:57 +0100

    added userInfo function to app.js for init

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import mainTemplate from './modules/main-template.js';
 import navbarMenu from './modules/navbar-menu.js';
 import sideMenu from './modules/side-menu.js';
+import userInfo from './modules/user-info.js';
 import footer from './modules/footer.js';
 import home from  './modules/home.js';
 
@@ -9,6 +10,7 @@ acs.init = () =>{
     mainTemplate.init();
     mainTemplate.addNavbarMenu(navbarMenu.render());
     mainTemplate.addSidebarMenu(sideMenu.render());
+    mainTemplate.addUserInfo(userInfo.render())
     mainTemplate.addFooter(footer.render());
     mainTemplate.addMain(home.render());
     mainTemplate.addEventListenerSideBar(),
