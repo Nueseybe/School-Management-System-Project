28bf0b3a9ffe55003a52ec839b2e6b40580c705a
Nueseybe <karakurtnuseybe@gmail.com>
2023-12-11 19:39:11 +0100

    edited coursesData values name

--- data/coursesData.js
+++ data/coursesData.js
@@ -1,49 +1,54 @@
 const coursesData = [
     {
-        className: "Web Development",
+        className: "Web Engineering",
         details: [{
-            course101A: [{
+            first: [{
                 courseName : "HTML",
                 time : "6 Weeks",
-                information: ""
+                information: "HTML stands for Hyper Text Markup Language /n HTML is the standard markup language for creating Web pages /n HTML describes the structure of a Web page /n HTML consists of a series of elements /n HTML elements tell the browser how to display the content" 
             }],
-            course101B:  [{
+            second:  [{
                 courseName : "CSS",
-                time : "4 Weeks",
-                information: ""
+                time : "4 Weeks", 
+                information: " CSS stands for Cascading Style Sheets /n CSS describes how HTML elements are to be displayed on screen, paper, or in other media /n CSS saves a lot of work. It can control the layout of multiple web pages all at once /n External stylesheets are stored in CSS files"
             }],
-            course101C:  [{
+            third:  [{
                 courseName : "JavaScript",
                 time : "10 Weeks",
-                information: ""
+                information: "JavaScript is the Programming Language for the Web. /n JavaScript can update and change both HTML and CSS. /n JavaScript can calculate, manipulate and validate data."
             }]
         }],
         className: "Python",
         details: [{
-            course102A: [{
+            first: [{
                 courseName : "Python OOP",
                 time : "8 Weeks",
                 information: ""
             }],
-            course101B:  [{
+            second:  [{
                 courseName : "PyQT5",
                 time : "3 Weeks",
                 information: ""
+            }],
+            third:  [{
+                courseName : "Scrapy",
+                time : "1 Weeks",
+                information: "Scrapy"
             }]
         }],
         className: "Data Science",
         details: [{
-            course101A: [{
+            first: [{
                 courseName : "Preprocessing",
                 time : "4 Weeks",
                 information: ""
             }],
-            course101B:  [{
+            second:  [{
                 courseName : "Litheratur",
                 time : "3 Weeks",
                 information: ""
             }],
-            course101C:  [{
+            third:  [{
                 courseName : "Modeling",
                 time : "7 Weeks",
                 information: ""
@@ -51,17 +56,17 @@ const coursesData = [
         }],
         className: "Aws-Cloud",
         details: [{
-            course101A: [{
+            first: [{
                 courseName : "JAM",
                 time : "6 Weeks",
                 information: ""
             }],
-            course101B:  [{
+            second:  [{
                 courseName : "EC2",
                 time : "6 Weeks",
                 information: ""
             }],
-            course101C:  [{
+            third:  [{
                 courseName : "ELB & ASG",
                 time : "6 Weeks",
                 information: ""
