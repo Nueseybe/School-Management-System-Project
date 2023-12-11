34d1b409956fa5a44ec91e1ec58064b459beaa1b
Nueseybe <karakurtnuseybe@gmail.com>
2023-12-11 19:19:45 +0100

    added user-info.js for user login display

---
+++ templates/user-info.js
@@ -0,0 +1,18 @@
+export default function template(){
+    return `
+        <div class="row">
+            <div class="col-md-5 title">
+                <h2> Welcome Admin</h2>
+            </div>
+            <div class="col-md-7 d-flex align-item-end justify-content-end ">
+                <div class="row">
+                    <img src="./images/student_icon/user-icon-05.png" style="height:10em">
+                </div>
+            </div>
+            <div class="row ">
+                <p class="d-flex justify-content-end">Admin</p>
+                <p class="d-flex justify-content-end">logout</p>
+            </div>
+        </div>
+    `
+}
