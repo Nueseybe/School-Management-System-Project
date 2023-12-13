291ba1f8bbe2d64e685f5255fc68fec130fbc837
Nueseybe <karakurtnuseybe@gmail.com>
2023-12-13 21:17:47 +0100

    edited add-student html codes

--- templates/add-student.js
+++ templates/add-student.js
@@ -1,24 +1,25 @@
-export default function template(){
-    return `
-        <div class="modal-dialog modal-dialog-centered" id="addStudent">
-            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addStudent" aria-hidden="true">
-            
-                <div class="modal-dialog">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
-                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                      Take information here
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                      <button type="button" class="btn btn-primary">Understood</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-        </div>
-    `
+export default function template(student){
+  return `
+    <div class="card content-column-body m-4" style="width: 18rem;">
+      <div class="card-body " id="studentForm">
+          <h4 class="card-title">Add Student</h4>
+         
+          
+            <div class="mb-3">
+              <label class="form-label">Name</label>
+              <input type="text" class="form-control" id="name" >
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Class</label>
+                <input type="text" class="form-control" id="class" >
+            </div>
+            <div class="mb-3">
+              <label class="form-label">Info</label>
+              <input type="text" class="form-control" id="info" >
+            </div>
+            <a class="btn btn-primary" id="saveBtn">Save</a>
+        
+      </div>
+  </div> 
+  `
 }
