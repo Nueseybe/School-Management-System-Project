e7c6d1a9307802cfc041635d96b4fd48b921829b
Nueseybe <karakurtnuseybe@gmail.com>
2023-12-13 19:48:25 +0100

    edited courses.data file

--- data/coursesData.js
+++ data/coursesData.js
@@ -2,83 +2,74 @@ const coursesData = [
     {
         className: "Web Engineering",
         details: [{
-            first: [{
-                courseName : "HTML",
-                time : "6 Weeks",
-                information: "HTML stands for Hyper Text Markup Language /n HTML is the standard markup language for creating Web pages /n HTML describes the structure of a Web page /n HTML consists of a series of elements /n HTML elements tell the browser how to display the content" 
-            }],
-            second:  [{
-                courseName : "CSS",
-                time : "4 Weeks", 
-                information: " CSS stands for Cascading Style Sheets /n CSS describes how HTML elements are to be displayed on screen, paper, or in other media /n CSS saves a lot of work. It can control the layout of multiple web pages all at once /n External stylesheets are stored in CSS files"
-            }],
-            third:  [{
-                courseName : "JavaScript",
-                time : "10 Weeks",
-                information: "JavaScript is the Programming Language for the Web. /n JavaScript can update and change both HTML and CSS. /n JavaScript can calculate, manipulate and validate data."
-            }]
+            courseName: "HTML",
+            time: "6 Weeks",
+            information: "HTML stands for Hyper Text Markup Language /n HTML is the standard markup language for creating Web pages /n HTML describes the structure of a Web page /n HTML consists of a series of elements /n HTML elements tell the browser how to display the content"
+        },
+        {
+            courseName: "CSS",
+            time: "4 Weeks",
+            information: " CSS stands for Cascading Style Sheets /n CSS describes how HTML elements are to be displayed on screen, paper, or in other media /n CSS saves a lot of work. It can control the layout of multiple web pages all at once /n External stylesheets are stored in CSS files"
+        },
+        {
+            courseName: "JavaScript",
+            time: "10 Weeks",
+            information: "JavaScript is the Programming Language for the Web. /n JavaScript can update and change both HTML and CSS. /n JavaScript can calculate, manipulate and validate data."
         }]
     },
     {
         className: "Python",
         details: [{
-            first: [{
-                courseName : "Python OOP",
-                time : "8 Weeks",
-                information: ""
-            }],
-            second:  [{
-                courseName : "PyQT5",
-                time : "3 Weeks",
-                information: ""
-            }],
-            third:  [{
-                courseName : "Scrapy",
-                time : "1 Weeks",
-                information: "Scrapy"
-            }]
+            courseName: "Python OOP",
+            time: "8 Weeks",
+            information: ""
+        },
+        {
+            courseName: "PyQT5",
+            time: "3 Weeks",
+            information: ""
+        },
+        {
+            courseName: "Scrapy",
+            time: "1 Weeks",
+            information: "Scrapy"
         }]
     },
     {
         className: "Data Science",
         details: [{
-            first: [{
-                courseName : "Preprocessing",
-                time : "4 Weeks",
-                information: ""
-            }],
-            second:  [{
-                courseName : "Litheratur",
-                time : "3 Weeks",
-                information: ""
-            }],
-            third:  [{
-                courseName : "Modeling",
-                time : "7 Weeks",
-                information: ""
-            }]
+            courseName: "Preprocessing",
+            time: "4 Weeks",
+            information: ""
+        }, 
+        {
+            courseName: "Litheratur",
+            time: "3 Weeks",
+            information: ""
+        }, 
+        {
+            courseName: "Modeling",
+            time: "7 Weeks",
+            information: ""
         }]
     },
     {
         className: "Aws-Cloud",
         details: [{
-            first: [{
-                courseName : "JAM",
-                time : "6 Weeks",
-                information: ""
-            }],
-            second:  [{
-                courseName : "EC2",
-                time : "6 Weeks",
-                information: ""
-            }],
-            third:  [{
-                courseName : "ELB & ASG",
-                time : "6 Weeks",
-                information: ""
-            }]
+            courseName: "JAM",
+            time: "6 Weeks",
+            information: ""
+        }, 
+        {
+            courseName: "EC2",
+            time: "6 Weeks",
+            information: ""
+        }, 
+        {
+            courseName: "ELB & ASG",
+            time: "6 Weeks",
+            information: ""
         }]
-
     }
 ]
 
